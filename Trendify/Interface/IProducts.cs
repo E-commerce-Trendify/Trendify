029c829ab9e43122243678c8ae7bacca6bf807db
abdarahman-shaheen <a.shaheen20001@gmail.com>
2023-08-29 13:02:13 +0300

    add service and interface for products

---
+++ Trendify/Interface/IProducts.cs
@@ -0,0 +1,23 @@
+﻿using Trendify.DTOs;
+
+namespace Trendify.Interface
+{
+    public interface IProducts
+    {
+        // Create
+        Task Create(ProductsDto products);
+
+        // GET All
+        Task<List<ProductsDtoView>> GetAllProducts();
+
+        // GET by ID
+        Task<ProductsDtoView> GetProductById(int id);
+
+        // UPDATE
+        Task Update(ProductsDto products , int id);
+
+        // DELET by ID
+        Task Delete(int id);
+    }
+}
+}
