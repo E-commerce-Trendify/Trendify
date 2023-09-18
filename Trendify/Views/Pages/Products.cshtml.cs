f3ebbbc2f273280a3cafc79d44f765c610688afb
abdarahman-shaheen <a.shaheen20001@gmail.com>
2023-09-18 17:02:32 +0300

    Razor-pages

---
+++ Trendify/Views/Pages/Products.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Trendify.Views.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
